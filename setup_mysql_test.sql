9af0348e9ad760de431f986449ba5188649cbe21
thomaskitaba <thomaskitaba@gmail.com>
2023-08-20 19:48:13 +0300

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- create test database
-CREATE DATABASE IF NOT EXIST hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create user and password for the test db
-CREATE USER IF NOT EXIST 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant priviledge on the database
 GRANT ALL PRIVILEGES ON `hbnb_test_db` TO 'hbnb_test'@'localhost';
 -- grant priviledges fot performance_schema
