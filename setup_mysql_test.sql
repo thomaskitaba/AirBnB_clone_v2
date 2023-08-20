60737623f39d20ecf3188721bc4294525f871164
thomaskitaba <thomaskitaba@gmail.com>
2023-08-20 19:07:03 +0300

    prepare mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,9 @@
 -- Create Database
-CREATE DATABASE IF NOT EXISTS Ihbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create new user
-CREATE USER hbnb_dev@localhost IDENTIFY BY hbnb_dev_pwd;
+CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFY BY `hbnb_dev_pwd`;
 -- Grant priviledge
-GRANT ALL PRIVILEGE ON hbnb_dev_db TO hbnb_dev@localhost;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO `hbnb_dev`@`localhost`;
 -- GRANT SELECT ON performance_schema TO hbnb_dev;
+GRANT SELECT ON hbnb_dev_db.* TO `hbnb_dev`@`localhost`;
+FLUSH PRIVILEGES;
