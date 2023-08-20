451dc2aa40c0afc3a83ee7368d33277ec438f393
thomaskitaba <thomaskitaba@gmail.com>
2023-08-20 19:50:59 +0300

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 -- create user and password for the test db
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant priviledge on the database
-GRANT ALL PRIVILEGES ON `hbnb_test_db` TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 -- grant priviledges fot performance_schema
-GRANT SELECT ON `performance_schema` TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
