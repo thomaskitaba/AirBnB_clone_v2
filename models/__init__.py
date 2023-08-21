5a013c504987b15b82b6ec93b3206bcec483ee02
thomaskitaba <thomaskitaba@gmail.com>
2023-08-21 20:44:40 +0300

    base model __dict__ modified

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
-
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
