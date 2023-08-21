8679c15d282825286cbbcb486c4a002804348924
thomaskitaba <thomaskitaba@gmail.com>
2023-08-21 15:00:17 +0300

    delete objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
 
-        if cls:
+        if not cls:
             return self.__objects
         else:
             __filtered = {}
