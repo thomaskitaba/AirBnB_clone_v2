407106488cab68b24248d99253e7cea806e94b3e
thomaskitaba <thomaskitaba@gmail.com>
2023-08-21 21:51:43 +0300

    updated_amenities

--- models/amenity.py
+++ models/amenity.py
@@ -5,13 +5,13 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
-
 class Amenity(BaseModel, Base):
     """Represents an amenity data set."""
     __tablename__ = 'amenities'
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         place_amenities = relationship(
             'Place',
