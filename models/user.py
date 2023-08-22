1863feb27cf438fe075fc4abb909d88392433ebd
thomaskitaba <thomaskitaba@gmail.com>
2023-08-22 15:06:59 +0300

    update user.py

--- models/user.py
+++ models/user.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
-from models.base_model.place import Place
-from models.base_model.review import Review
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
