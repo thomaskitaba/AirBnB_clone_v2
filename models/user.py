b24dfba35745ff1792c0c793349540b351c0dc33
thomaskitaba <thomaskitaba@gmail.com>
2023-08-22 12:59:07 +0300

    upda

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""User class"""
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -8,7 +8,7 @@ from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """ Define class User with all its attributes"""
     __tablename__ = 'users'
     email = Column(
         String(128), nullable=False
