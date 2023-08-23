6d9a450a20b827e1e0d881f59ac02847559cd73b
thomaskitaba <thomaskitaba@gmail.com>
2023-08-23 23:33:18 +0300

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A unit test module for the console (command interpreter).
+"""
+    Test for console.py
 """
 import json
 import MySQLdb
@@ -8,7 +9,6 @@ import sqlalchemy
 import unittest
 from io import StringIO
 from unittest.mock import patch
-
 from console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
@@ -17,7 +17,8 @@ from tests import clear_stream
 
 
 class TestHBNBCommand(unittest.TestCase):
-    """Represents the test class for the HBNBCommand class.
+    """
+    Test.
     """
     @unittest.skipIf(
         os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
