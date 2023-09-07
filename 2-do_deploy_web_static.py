9daa2a2323b6e12b081de31343b9d52a46bbd28d
thomaskitaba <thomaskitaba@gmail.com>
2023-09-07 15:53:06 +0300

    correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -43,7 +43,6 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
@@ -52,20 +51,20 @@ def do_deploy(archive_path):
     if run("sudo mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("sudo rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
+    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
