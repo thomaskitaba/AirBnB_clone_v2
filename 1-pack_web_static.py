fdcd16c7a81c31a67fe2c864018d6459e5368656
thomaskitaba <thomaskitaba@gmail.com>
2023-09-07 13:58:49 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -32,10 +32,10 @@ def do_pack():
         cur_time.second
     )
     try:
-        print("Packing web_static to {}".format(output))
+        # print("Packing web_static to {}".format(output))
         local("tar -cvzf {} web_static".format(output))
         archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+        # print("web_static packed: {} -> {} Bytes".format(output, archize_size))
     except Exception:
         output = None
     return output
