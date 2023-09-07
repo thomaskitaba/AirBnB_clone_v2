2dff76a05fb73089383edb54591985320be44425
thomaskitaba <thomaskitaba@gmail.com>
2023-09-07 15:37:38 +0300

    correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,45 +12,53 @@ web_static_<year><month><day><hour><minute><second>.tgz
 The function do_pack must return the archive path if the
 archive
 """
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
+
 
 env.hosts = ["34.229.69.114", "100.26.122.201"]
 """ list of host ip address """
 
 
-def do_deploy(archive_path):
-    """ deploy webstatic to servers
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not isdir("versions"):
+        local("mkdir -p versions")
+    cur_time = datetime.now()
+    archive_time = cur_time.strftime("%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{archive_time}.tgz"
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    try:
+        local("tar -cvzf {} web_static".format(archive_name))
+    except Exception:
+        archive_name = None
+    return archive_name
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
---
+++ 2-do_deploy_web_static_03.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Write a Fabric script that generates a .tgz archive from the contents
+of the web_static folder of your AirBnB Clone repo, using the
+function do_pack.
+Prototype: def do_pack():
+All files in the folder web_static must be added to the final archive
+All archives must be stored in the folder versions (your function
+should create this folder if it doesn’t exist)
+The name of the archive created must be
+web_static_<year><month><day><hour><minute><second>.tgz
+The function do_pack must return the archive path if the
+archive
+"""
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+
+env.hosts = ["34.229.69.114", "100.26.122.201"]
+""" list of host ip address """
+
+
+def do_deploy(archive_path):
+    """ deploy webstatic to servers
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
