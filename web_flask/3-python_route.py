7c7d5269f4ee0ba0a09884d9de79cbcf3303142d
thomaskitaba <thomaskitaba@gmail.com>
2023-09-21 15:34:10 +0300

    update

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """  return Hello HBNB! """
+    return " “Hello HBNB!”"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ return HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    """ return C and text provided"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashed=False)
+def python(text):
+    """ diplay Python and text value """
+    return f"Python {text.replace('_'.' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
