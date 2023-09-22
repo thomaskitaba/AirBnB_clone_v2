ea47a140218b9dfbc421597628a524feef7fe603
thomaskitaba <thomaskitaba@gmail.com>
2023-09-22 14:08:10 +0300

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,5 +18,14 @@ def states_list():
     return ("8-cities_by_states.html", all_states=all_states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+  """closes the storage on teardown"""
+  storage.close()
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port='5000')
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port='5000')
