752f39a79bd692caf34e9db60d70b0e21dd8ca10
thomaskitaba <thomaskitaba@gmail.com>
2023-09-23 19:45:10 +0300

    correction to state

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
+
 from models.base_model import BaseModel, Base
 from models.city import City
 
@@ -11,23 +11,16 @@ from models.city import City
 class State(BaseModel, Base):
     """ Define State class and its attributes"""
     __tablename__ = 'states'
-
-    # if storage_type == 'db'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
         cities = relationship(
             'City',
             cascade='all, delete, delete-orphan',
             backref='state'
         )
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         @property
         def cities(self):
             """Returns the cities in this State"""
