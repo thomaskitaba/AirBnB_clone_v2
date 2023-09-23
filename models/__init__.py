1f118b265f72e5c887a85909d39438b64bd08bdf
thomaskitaba <thomaskitaba@gmail.com>
2023-09-23 17:09:42 +0300

    7-states_list.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
+"""
+initialize the models package
+"""
 
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
+from os import getenv
 
-# Check the value of HBNB_TYPE_STORAGE environment variable
-storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
-# Choose the appropriate storage class based on the value
-if storage_type == 'db':
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
-# Load the data using the storage instance
 storage.reload()
-
-# storage = DBStorage() if os.getenv(
-#     'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
-# """A unique FileStorage/DBStorage instance for all models.
-# """
-# storage.reload()
