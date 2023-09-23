f9b41ee35cff83639d8b80e0e1b241f2edf368ae
thomaskitaba <thomaskitaba@gmail.com>
2023-09-23 22:06:01 +0300

    added storage = 0 to __init__ models

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ from models.engine.file_storage import FileStorage
 
 # Check the value of HBNB_TYPE_STORAGE environment variable
 storage_type = os.getenv('HBNB_TYPE_STORAGE')
-
+storage = ''
 # Choose the appropriate storage class based on the value
 if storage_type == 'db':
     storage = DBStorage()
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,8 +16,7 @@ def states_list():
         ...
         ]
     """
-    #all_states = sorted(list(storage.all('State')), key=lambda x: x.name)
-    all_states = storage.all("State")
+    all_states = sorted(list(storage.all('State')), key=lambda x: x.name)
 
     return render_template("7-states_list.html", all_states=all_states)
 
