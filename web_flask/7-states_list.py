f5042f785252e6f63f2875e36bf84d631e77646d
thomaskitaba <thomaskitaba@gmail.com>
2023-09-24 17:50:54 +0300

    correction to 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,8 +15,8 @@ def states_list():
         ...
         ]
     """
-    # all_states = sorted(list(storage.all('State')), key=lambda x: x.name)
-    all_states = storage.all('State').value
+    all_states = sorted(list(storage.all('State')), key=lambda x: x.name)
+    # all_states = storage.all('State').value
     return render_template("7-states_list.html", all_states=all_states)
 
 
