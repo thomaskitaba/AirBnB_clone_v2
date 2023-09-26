122ef945c6ce736e41f7e0aeda5e566b6bf01df4
thomaskitaba <thomaskitaba@gmail.com>
2023-09-26 15:52:37 +0300

    update_7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-""" 8- list states """
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
 from models import *
 from models import storage
-
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ H1 = States, ul states State: <state.id>: <B><state.name></B>
-        list(storage.all('States')) output: [<State object>, <State object>
-        ...
-        ]
-    """
-    all_states = sorted(list(storage.all('State')), key=lambda x: x.name)
-    # all_states = storage.all('State').value
-    return render_template("7-states_list.html", all_states=all_states)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
@@ -25,6 +21,5 @@ def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
